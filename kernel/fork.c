7aec3ad2ea205aa81004ce219eb204c3d24fbe9a
derflacco <paul89rulez@gmail.com>
2020-06-18 23:35:44 +0200

    kernel: fork.c: remove DEVFREQ_MSM_LLCCBW boosting

--- kernel/fork.c
+++ kernel/fork.c
@@ -2082,7 +2082,6 @@ long _do_fork(unsigned long clone_flags,
 	if (is_zygote_pid(current->pid)) {
 		cpu_input_boost_kick_max(150);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 150);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 150);
 	}
 
 	/*
