2b280f806ea85513d20a9bec4309ba6bb29dbdc4
derflacco <paul89rulez@gmail.com>
2020-07-13 01:25:31 +0200

    fenix_defconfig: regen

--- make_kerna_clang.sh
+++ make_kerna_clang.sh
@@ -35,11 +35,7 @@ PATH="/home/derflacco/toolchains/proton-clang-20200613/bin:$PATH"
 export CROSS_COMPILE=aarch64-linux-gnu-
 make $defconfig CC=clang O=output/
 
-make -j$(nproc --all) 
-                NM=llvm-nm \
-		OBJCOPY=llvm-objcopy \
-                LD=ld.lld \ 
-                CC="ccache clang -fcolor-diagnostics -Qunused-arguments" O=output/
+make -j$(nproc --all) NM="llvm-nm" OBJCOPY="llvm-objcopy" LD="ld.lld"  CC="ccache clang -fcolor-diagnostics -Qunused-arguments" O=output/
 
 make_zip()
 {
